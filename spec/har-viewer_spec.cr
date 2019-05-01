957db2634bfc6d0cb33688b8f70f5357894c8cd4
weaming <garden.yuen@gmail.com>
2019-05-01 14:17:52 +0800

    init

---
+++ spec/har-viewer_spec.cr
@@ -0,0 +1,9 @@
+require "./spec_helper"
+
+describe Har::Viewer do
+  # TODO: Write tests
+
+  it "works" do
+    false.should eq(true)
+  end
+end
---
+++ src/har-viewer.cr
@@ -0,0 +1,6 @@
+# TODO: Write documentation for `Har::Viewer`
+module Har::Viewer
+  VERSION = "0.1.0"
+
+  # TODO: Put your code here
+end
