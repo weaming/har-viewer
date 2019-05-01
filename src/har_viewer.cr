769ff245fc0c511718499bd5087671be3076db41
weaming <garden.yuen@gmail.com>
2019-05-01 18:35:58 +0800

    add class Renderer

--- src/har_viewer.cr
+++ src/har_viewer.cr
@@ -6,31 +6,51 @@ require "./helper"
 module HARViewer
   include Helper
   extend self
-
   VERSION = "0.1.0"
 
-  def read_har(file : String) : HAR::Log
-    if file == "-"
-      file = "/dev/stdin"
-    end
-    log = HAR.from_file(file)
-    log
-  end
+  class Renderer
+    getter path : String
+    getter log : (HAR::Log | Nil)
 
-  def render_entry(entries : Array(HAR::Entries)) : String
-    # head = Kilt.render("src/templates/base.slang")
-    head = ""
-    body = Kilt.render("src/templates/entries.slang")
-    head + body
+    def initialize(@path : String)
+      self.read_har
+    end
+
+    def read_har
+      if @path == "-"
+        @path = "/dev/stdin"
+      end
+      @log = HAR.from_file(path)
+
+      if ENV["DEBUG"]?
+        puts @log.to_pretty_json "  "
+      end
+    end
+
+    def render : String
+      head = self.render_head
+      # avoid compile fail
+      log = @log
+      if log
+        body = self.render_entries log.entries
+      else
+        body = ""
+      end
+      head + body
+    end
+
+    def render_entries(entries : Array(HAR::Entries)) : String
+      Kilt.render("src/templates/entries.slang")
+    end
+
+    def render_head : String
+      ""
+    end
   end
 
   def main
-    log = read_har "/dev/stdin"
-    if ENV["DEBUG"]?
-      puts log.to_pretty_json "  "
-    end
-
-    html = render_entry log.entries
+    renderer = Renderer.new "-"
+    html = renderer.render
     FileIO.write_file CLI.argv_first("missing OUTPUT"), html.to_slice
   end
 end
